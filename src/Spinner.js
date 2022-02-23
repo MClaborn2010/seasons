b8143518990fa49b5a4b3646877f688e1ee7145e
Michael Claborn <mclaborn2010@gmail.com>
2022-02-22 21:59:48 -0600

    added loading spinner when requesting location, refactored code

---
+++ src/Spinner.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Spinner = (props) => {
+  return (
+    <div class="ui active dimmer">
+      <div class="ui loader big text">
+        {props.message}
+        <i class="map marker alternate icon"></i>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
